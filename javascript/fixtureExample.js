7eff3d4394883529b8a6ba31b1fdcf05c35ec93f
Mercedes Bernard <mercedesrbernard@gmail.com>
2021-04-20 11:28:28 -0500

    add js fixture example

---
+++ javascript/fixtureExample.js
@@ -0,0 +1,28 @@
+import faker from "faker";
+import { randomInteger } from "lib";
+
+const ROLES = ["Development", "Design", "Growth"];
+
+const build = (attributes, i) => {
+  const index = i !== null && i !== undefined ? i : 1;
+  return {
+    id: `person-${index}`,
+    roles: [ROLES[randomInteger(0, 2)]],
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    ...attributes,
+  };
+};
+
+const buildList = (amount, attributes) => {
+  let parsedAttributes = attributes || {};
+  const list = [];
+  for (var i = 0; i < amount; i++) {
+    list.push(build(parsedAttributes, i));
+  }
+  return list;
+};
+
+const fixture = { build, buildList };
+
+export default fixture;
